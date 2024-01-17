76f525b6ef3f3f2aa275039a4b40f239a7ffe256
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-01-17 17:17:50 +0100

    Mi primer commit desde git

---
+++ src/trabajo/Par.java
@@ -0,0 +1,18 @@
+package trabajo;
+
+public class Par {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<100;i++) {
+			if(i%2==0) {
+				System.out.println(i+" es par");
+			} else {
+				System.out.println(i+" es impar");
+				
+			}
+		}
+
+	}
+
+}
